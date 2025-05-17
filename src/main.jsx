38d33fc633470a90cde94134263480147674416f
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-05-17 23:52:00 +0530

    addCreated base routes using React Router  routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { FirebaseProvider } from "./context/Firebase";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <FirebaseProvider>
-      <App />
-    </FirebaseProvider>
+    <BrowserRouter>
+      <FirebaseProvider>
+        <App />
+      </FirebaseProvider>
+    </BrowserRouter>
   </StrictMode>
 );
