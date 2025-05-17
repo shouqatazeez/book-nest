7c8566c57373d5963902800054817c6232412744
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-05-18 00:46:42 +0530

    add register page with Firebase authentication

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext } from "react";
 import { initializeApp } from "firebase/app";
-
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 const FirebaseContext = createContext(null);
 
 const firebaseConfig = {
@@ -17,7 +17,14 @@ export const useFirebase = () => useContext(FirebaseContext);
 
 // eslint-disable-next-line no-unused-vars
 const firebaseApp = initializeApp(firebaseConfig);
+const firebaseAuth = getAuth(firebaseApp);
 
 export const FirebaseProvider = (props) => {
-  return <FirebaseContext.Provider>{props.children}</FirebaseContext.Provider>;
+  const signupUserWithEmailAndPassword = (email, password) =>
+    createUserWithEmailAndPassword(firebaseAuth, email, password);
+  return (
+    <FirebaseContext.Provider value={{ signupUserWithEmailAndPassword }}>
+      {props.children}
+    </FirebaseContext.Provider>
+  );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useFirebase } from "../context/Firebase";
 
 const RegisterPage = () => {
+  const firebase = useFirebase();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("signin up a user");
+    // const result = await firebase.signupUserWithEmailAndPassword(
+    const result = await firebase.signupUserWithEmailAndPassword(
+      email,
+      password
+    );
+
+    console.log("successfull", result);
+  };
+
   return (
     <div className="container mt-5 ">
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            placeholder="Enter email"
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -16,13 +39,15 @@ const RegisterPage = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Control
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="password"
+            placeholder="Password"
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
-          Submit
+          Create Account
         </Button>
       </Form>
     </div>
